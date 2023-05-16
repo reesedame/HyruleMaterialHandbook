c0ccb9e89f3a0b62bd0b74e332bf8749bc8d0f07
Reese Dame <reesedame@gmail.com>
2023-05-16 10:24:26 -0700

    Add font + update styling

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,19 +3,20 @@ import "./HomePage.css";
 
 function HomePage() {
 	return (
-		<div className="container">
+		<div className="home-page-container">
 			<div className="title">
-				<h1>Legend of Zelda: Breath of the Wild</h1>
 				<h1>Hyrule Material Handbook</h1>
+				<h2> for Legend of Zelda: Breath of the Wild</h2>
 			</div>
 			<div className="home-page-content">
+				<h3 id="welcome-header">Welcome!</h3>
 				<p>
-					Welcome! Here you will find all of the materials featured in the game
-					Breath of the Wild. Clicking on the show details button will reveal
-					additional information about each material such as common locations,
-					how many hearts it will give you, and what effects it has after
-					cooking. I hope you will find this information useful as you traverse
-					through the open world game. Enjoy and good luck on your adventure!
+					Here you will find all of the materials featured in the game Breath of
+					the Wild. Clicking on the show details button will reveal additional
+					information about each material such as common locations, how many
+					hearts it will give you, and what effects it has after cooking. I hope
+					you will find this information useful as you traverse through the open
+					world game. Enjoy and good luck on your adventure!
 				</p>
 				<Link to="/materials">View Materials</Link>
 			</div>
